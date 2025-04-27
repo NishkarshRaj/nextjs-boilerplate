668ee293b759d69fbe55862b756b212b04822c5e
NishkarshRaj <nishkarshraj000@gmail.com>
2025-04-27 17:59:02 -0400

    [JIRA-XXXX] adding button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,6 +37,14 @@ export default function Home() {
           >
             AIOps Services
           </a>
+          <a
+            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
+            href="https://backstage.io/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Backstage
+          </a>
         </div>
       </main>
     </div>
